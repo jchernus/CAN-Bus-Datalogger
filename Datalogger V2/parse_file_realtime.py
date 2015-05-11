8829765081697b1164727f5c1297e0be7a7aa0ac
Julia Chernushevich <jchernushevich@lk56cm-001.sk.pmparts.com>
2015-05-11 15:07:08 -0600

    Deleted Arduino scripts, corrected password

--- Datalogger V2/parse_file_realtime.py
+++ Datalogger V2/parse_file_realtime.py
@@ -12,7 +12,7 @@ previous_date = "", previous_time = 0
 path = ""
 
 def twos_comp(val, bits):
-    """compute the 2's compliment of int value val"""
+    #compute the 2's compliment of int value val
     val = int(val)
     if (val & (1 << (bits - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
         val = val - (1 << bits)        # compute negative value
