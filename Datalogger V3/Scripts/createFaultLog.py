455138ba057750aa0b4b92c5cfd2cd2cffeff464
Julia Chernushevich <julia.chernushevich@gmail.com>
2015-11-27 14:47:46 -0600

    Added authentication to Sevcon Fault script

--- Datalogger V3/Scripts/createFaultLog.py
+++ Datalogger V3/Scripts/createFaultLog.py
@@ -2,8 +2,45 @@
 
 import sqlite3, subprocess, re, os
 
+faultID = hours = minutes = 0
+
 csv_path = "/var/tmp/logs/"
 db_path = "/data/databases/FaultDictionary.db"
+
+def parse_msg (line):
+        
+        global faultID, hours, minutes
+        message = ""
+        
+        if (" 12 41 01 " in line):                      #single message
+                #parse CAN data
+                try:
+                        data = line.split("  ")[4][12:17].replace(" ","")
+                        faultID = int(data[2:] + data[:2], 16)
+                except:
+                        message = "Error: Unable to parse data. Data: " + line
+                
+        elif (" 12 41 02 " in line):                      #single message
+                #parse CAN data
+                try:
+                        data = line.split("  ")[4][12:17].replace(" ","")
+                        hours = int(data[2:] + data[:2], 16)
+                except:
+                        message = "Error: Unable to parse data. Data: " + line
+                
+        elif (" 12 41 03 " in line):                      #single message
+                #parse CAN data
+                try:
+                        minutes = int(line.split("  ")[4][12:14], 16)
+                except:
+                        message =  "Error: Unable to parse data. Data: " + line
+
+        elif ("581   [8]  80" in line):                           #crashed
+                message = "Error: Crashed motor controller. Please try again."
+        else:
+                message = "Error: Unexpected message format, cannot decode reply from motor controller. Data: " + line
+
+        return message
 
 conn = sqlite3.connect(db_path)
 curs = conn.cursor()
@@ -24,9 +61,15 @@ with open(csv_path, 'a+') as file:
         file.write("Time:," + current_date[11:] + "\n\n")
 
         #SEVCON Fault Retreival
-        file.write('Fault ID, Hours Ago [h], Minutes & Seconds Ago\n')
+        file.write('Hours Ago [h], Minutes & Seconds Ago, Fault ID, Fault Name, Fault Cause, Fault Remedy\n')
+        
+        #send password to gain access
+        p = subprocess.Popen("cansend can0 601#2B005002DF4BEFFA", cwd="/data/can-utils/", stdout=subprocess.PIPE, shell=True)
+        p = subprocess.Popen("candump -t A -n 1 -T 100 can0,581:7ff", cwd="/data/can-utils/", stdout=subprocess.PIPE, shell=True)
+        (output, err) = p.communicate()
         
         for num in range(0,40):
+               
                 #select event through 4111
                 p = subprocess.Popen("cansend can0 601#2B114100" + hex(num)[2:].zfill(2) + "00EFFA", cwd="/data/can-utils/", stdout=subprocess.PIPE, shell=True)
                 p = subprocess.Popen("candump -t A -n 1 -T 100 can0,581:7ff", cwd="/data/can-utils/", stdout=subprocess.PIPE, shell=True)
@@ -37,50 +80,28 @@ with open(csv_path, 'a+') as file:
                 p = subprocess.Popen("candump -t A -n 3 -T 500 can0,581:7ff", cwd="/data/can-utils/", stdout=subprocess.PIPE, shell=True)
                 (output, err) = p.communicate()
 
+                message = ""
                 if len(output) > 0:  # got the response message
 
-                        lines = output.split("\n").strip()
+                        lines = output.strip().split("\n")
 
                         for line in lines:
-                                if ("581   [8]  4F 12 41 01" in line):                      #single message
-                                        #parse CAN data
-                                        try:
-                                                data = lines[0].split("  ")[3][16:21].strip()
-                                                faultID = int(data[3:] + data[:2], 16)
-                                        except:
-                                                print "Error: Unable to parse data. Data: " + faultData
-                                        
-                                if ("581   [8]  4F 12 41 02" in line):                      #single message
-                                        #parse CAN data
-                                        try:
-                                                data = lines[0].split("  ")[3][16:21].strip()
-                                                hours = int(data[3:] + data[:2], 16)
-                                        except:
-                                                print "Error: Unable to parse data. Data: " + faultData
-                                        
-                                if ("581   [8]  4F 12 41 03" in line):                      #single message
-                                        #parse CAN data
-                                        try:
-                                                minutes = int(lines[0].split("  ")[3][16:18].strip(), 16)
-                                        except:
-                                                print "Error: Unable to parse data. Data: " + faultData
-
-                                elif ("581   [8]  80" in output):                                         #crashed
-                                        print "Error: Crashed motor controller. Please do a key cycle to recover, and then try again."
-                                else:
-                                        print "Error: Unexpected message format, cannot decode reply from motor controller."
-
-                        #query database for fault description and remedy
-                        curs.execute("SELECT * FROM FaultDictionary LIMIT 1 WHERE faultID = '" + int(faultID, 16) + "'")   #faultID, message, description, action
-                        data = curs.fetch()
-
-                        #record to csv
-                        file.write(hours + "," + minutes + "," + data[0] + "," + data[1] + "," + data[2] + "," + data[3] + "\n")
+                                message = parse_msg(line)
 
                 else:
-                        print "Error: Did not receive reply from motor controller."
+                        message = "Error: Did not receive reply from motor controller."
+                
+                if message == "":      
+                        #query database for fault description and remedy
+                        curs.execute("SELECT * FROM FaultDictionary WHERE faultID = '" + str(faultID) + "'")   #faultID, message, description, action
+                        for row in curs: #only ever one, since unique
+                                #record to csv
+                                file.write(str(hours) + "," + str(minutes) + "," + str(row[0]) + "," + str(row[1]) + "," + str(row[2]) + "," + str(row[3]) + "\n")
+                                #print str(hours) + "," + str(minutes) + "," + str(row[0]) + "," + str(row[1]) + "," + str(row[2]) + "," + str(row[3]) + "\n"
+                else:
+                        print message
 
         #BMS Fault Retrieval
-        file.write('Fault ID\n')
+        file.write('\n\nFault ID\n')
 
 conn.close()
